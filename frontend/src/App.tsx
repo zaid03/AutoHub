bec7676bde86bfac19e2bdb852b355a622eda889
zaid03 <terguyzaid@gmail.com>
2026-03-16 03:23:35 +0000

    new compnent for footer with related routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/header" element={<Header />} />  {/* to delete later */}
-        
+        <Route path="/footer" element={<Footer />} />  {/* to delete later */}
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <>
+            <div className="test">
+                <h1>test</h1>
+            </div>
+        </>
+    );
+};
+
+export default Footer;

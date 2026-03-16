804cda669761394c890dc7e1923e6459ff1ce3e4
zaid03 <terguyzaid@gmail.com>
2026-03-16 00:12:51 +0000

    added routing and changed default page to load the home + working on header

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const header = () => {
+    return (
+        <div className="test">
+            <h1>header working</h1>
+        </div>
+    );
+};
+
+export default header;
---
+++ frontend/src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+const home = () => {
+    return (
+        <div>
+            <h1 className="test">test</h1>
+        </div>
+    );
+};
+
+export default home;

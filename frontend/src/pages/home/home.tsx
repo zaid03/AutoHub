9246032354fe95d66c259580f242a5224bdb0b98
zaid03 <terguyzaid@gmail.com>
2026-04-12 17:42:29 +0100

    added saerch container + its options + style

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -7,8 +7,44 @@ const home = () => {
     return (
         <>
             <Header />
-            <div>
-                <h1 className="test">test</h1>
+            <div className="main-container">
+                <div className="search-container">
+                    <div className="search-params">
+                        <div className="search-options">
+                            <div className="first-row">
+                                <select>
+                                    <option disabled selected hidden>marque</option>
+                                    <option>test</option>
+                                    <option>test</option>
+                                    <option>test</option>
+                                </select>
+                                <select>
+                                    <option disabled selected hidden>model</option>
+                                    <option>test</option>
+                                    <option>test</option>
+                                    <option>test</option>
+                                </select>
+                            </div>
+                            <div className="second-row">
+                                <input 
+                                    type="number"
+                                    placeholder="Prix minimum" />
+                                <input 
+                                    type="number"
+                                    placeholder="Prix maximum" />
+                            </div>
+                            <div className="third-row">
+                                <input 
+                                    type="number"
+                                    placeholder="Kilométrage minimum" />
+                                <input 
+                                    type="number"
+                                    placeholder="Kilométrage maximum" />
+                            </div>
+                        </div>
+                        <button className="blue-btn">Rechercher</button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>

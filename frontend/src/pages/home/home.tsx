9319f8fcc901cfc70288041a1aff0e9e3e49831e
zaid03 <terguyzaid@gmail.com>
2026-04-18 16:59:09 +0100

    changed brand api to select logo too + added most popular brand + model sections

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -13,13 +13,13 @@ const home = () => {
                         <div className="search-options">
                             <div className="first-row">
                                 <select>
-                                    <option disabled selected hidden>marque</option>
+                                    <option disabled selected hidden>Brand</option>
                                     <option>test</option>
                                     <option>test</option>
                                     <option>test</option>
                                 </select>
                                 <select>
-                                    <option disabled selected hidden>model</option>
+                                    <option disabled selected hidden>Model</option>
                                     <option>test</option>
                                     <option>test</option>
                                     <option>test</option>
@@ -28,21 +28,67 @@ const home = () => {
                             <div className="second-row">
                                 <input 
                                     type="number"
-                                    placeholder="Prix minimum" />
+                                    placeholder="Minimum price"
+                                    min={0} />
                                 <input 
                                     type="number"
-                                    placeholder="Prix maximum" />
+                                    placeholder="Maximum prix"
+                                    min={0} />
                             </div>
                             <div className="third-row">
                                 <input 
                                     type="number"
-                                    placeholder="Kilométrage minimum" />
+                                    placeholder="Minimum kilometres"
+                                    min={0} />
                                 <input 
                                     type="number"
-                                    placeholder="Kilométrage maximum" />
+                                    placeholder="Maximum kilometres"
+                                    min={0} />
                             </div>
                         </div>
-                        <button className="blue-btn">Rechercher</button>
+                        <button className="blue-btn">Search</button>
+                    </div>
+                </div>
+            </div>
+            <div className="second-container">
+                <div className="first-stat">
+                    <div className="title-area">
+                        <i className="fa-solid fa-square"></i>
+                        <span className="title-style">Most popular brands</span>
+                    </div>
+                    <div className="stat-data">
+                        <div className="stat">
+                            <img src="/images/audi.png" alt="audi logo" />
+                            <span className="span-style">Mercedes</span>
+                        </div>
+                        <div className="stat">
+                            <img src="/images/audi.png" alt="audi logo" />
+                            <span className="span-style">Audi</span>
+                        </div>
+                        <div className="stat">
+                            <img src="/images/audi.png" alt="audi logo" />
+                            <span className="span-style">Audi</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="second-stat">
+                    <div className="title-area">
+                        <i className="fa-solid fa-square"></i>
+                        <span className="title-style">Most popular models</span>
+                    </div>
+                    <div className="stat-data">
+                        <div className="stat">
+                            <img src="/images/audi.png" alt="audi logo" />
+                            <span className="span-style">Audi</span>
+                        </div>
+                        <div className="stat">
+                            <img src="/images/audi.png" alt="audi logo" />
+                            <span className="span-style">Audi</span>
+                        </div>
+                        <div className="stat">
+                            <img src="/images/audi.png" alt="audi logo" />
+                            <span className="span-style">Audi</span>
+                        </div>
                     </div>
                 </div>
             </div>

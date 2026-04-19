37056681dc5f9539aad5126f3c85112b98dac902
zaid03 <terguyzaid@gmail.com>
2026-04-19 18:41:03 +0100

    changed url for all deviced on network to work + loaded top brands and models data into their container

--- Backend/models/Brands.js
+++ Backend/models/Brands.js
@@ -3,7 +3,7 @@ const connectDb = require('../config/db');
 //to select brand names
 const getBrands = async () => {
     const connection = await connectDb();
-    const [brands] = await connection.execute('SELECT * FROM brands');
+    const [brands] = await connection.execute('SELECT id, name FROM brands');
     await connection.end();
     return brands;
 };
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./home.css";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 
-const home = () => {
+const Home = () => {
+    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+    const [brands, setBrands] = useState<Array<{id: string; name: String}>>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [models, setModels] = useState<Array<{model_name: string}>>([]);
+    const [errorModels, setErrorModels] = useState<string | null>(null);
+    const [topBrands, setTopBrands] = useState<Array<{name: string; logo: string;}>>([]);
+    const [topModels, setTopModels] = useState<Array<{model_name: string; logo: string}>>([]);
+    const [errorTopBrands, setErrorTopBrands] = useState<string | null>(null);
+    const [errorTopModels, setErrorTopModels] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const BrandResponse = await fetch(`${API_URL}/api/brands`);
+                if (!BrandResponse.ok) throw Error("failed to fetch brands");
+                const data = await BrandResponse.json();
+                setBrands(data.data);
+            } catch (error) {
+                const errorMessage = error instanceof Error ? error.message : "An error occurred";
+                setError(errorMessage);
+            }
+
+            try {
+                const TopBrandResponse = await fetch(`${API_URL}/api/brands/top-brands`);
+                if (!TopBrandResponse.ok) throw Error("failed to fetch top brands");
+                const data = await TopBrandResponse.json();
+                setTopBrands(data.data);
+            } catch (error) {
+                const errorMessage = error instanceof Error ? error.message : "An error occurred";
+                setErrorTopBrands(errorMessage);
+            }
+
+            try {
+                const TopModelsResponse = await fetch(`${API_URL}/api/models/top-models`);
+                if (!TopModelsResponse.ok) throw Error("failed to fetch top brands");
+                const data = await TopModelsResponse.json();
+                setTopModels(data.data);
+            } catch (error) {
+                const errorMessage = error instanceof Error ? error.message : "An error occurred";
+                setErrorTopModels(errorMessage);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const handleBrandChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const brand_id = e.target.value;
+        try {
+            const response = await fetch(`${API_URL}/api/models/${brand_id}`);
+            if (!response.ok) throw Error("failed to fetch models");
+            const data = await response.json();
+            setModels(data.data);
+        } catch(error) {
+            const errorMessage = error instanceof Error ? error.message : "An error occurred";
+            setErrorModels(errorMessage)
+        }
+    }
     return (
         <>
             <Header />
@@ -12,18 +70,30 @@ const home = () => {
                     <div className="search-params">
                         <div className="search-options">
                             <div className="first-row">
-                                <select>
-                                    <option disabled selected hidden>Brand</option>
-                                    <option>test</option>
-                                    <option>test</option>
-                                    <option>test</option>
-                                </select>
-                                <select>
-                                    <option disabled selected hidden>Model</option>
-                                    <option>test</option>
-                                    <option>test</option>
-                                    <option>test</option>
-                                </select>
+                                {error ? (
+                                    <select disabled>
+                                        <option className="Error">{error}</option>
+                                    </select>
+                                ) : (
+                                    <select onChange={handleBrandChange} defaultValue="">
+                                        <option disabled value="">Brand</option>
+                                        {brands.map((brand) => (
+                                            <option key={brand.id} value={brand.id}>{brand.name}</option>
+                                        ))}
+                                    </select>
+                                )}
+                                {errorModels ? (
+                                    <select disabled>
+                                        <option className="Error">{errorModels}</option>
+                                    </select>
+                                ): (
+                                    <select defaultValue="">
+                                        <option disabled value="">Models</option>
+                                        {models.map((model) => (
+                                            <option key={model.model_name} value={model.model_name}>{model.model_name}</option>
+                                        ))}
+                                    </select>
+                                )}
                             </div>
                             <div className="second-row">
                                 <input 
@@ -57,18 +127,18 @@ const home = () => {
                         <span className="title-style">Most popular brands</span>
                     </div>
                     <div className="stat-data">
-                        <div className="stat">
-                            <img src="/images/audi.png" alt="audi logo" />
-                            <span className="span-style">Mercedes</span>
-                        </div>
-                        <div className="stat">
-                            <img src="/images/audi.png" alt="audi logo" />
-                            <span className="span-style">Audi</span>
-                        </div>
-                        <div className="stat">
-                            <img src="/images/audi.png" alt="audi logo" />
-                            <span className="span-style">Audi</span>
-                        </div>
+                        {errorTopBrands? (
+                            <div className="stat">
+                                <span className="Error">{errorTopBrands}</span>
+                            </div>
+                        ): (
+                            topBrands.map((brand) => (
+                                <div key={brand.name} className="stat">
+                                    <img src={brand.logo} alt={brand.name} />
+                                    <span className="span-style">{brand?.name}</span>
+                                </div>
+                            ))
+                        )}
                     </div>
                 </div>
                 <div className="second-stat">
@@ -77,18 +147,18 @@ const home = () => {
                         <span className="title-style">Most popular models</span>
                     </div>
                     <div className="stat-data">
-                        <div className="stat">
-                            <img src="/images/audi.png" alt="audi logo" />
-                            <span className="span-style">Audi</span>
-                        </div>
-                        <div className="stat">
-                            <img src="/images/audi.png" alt="audi logo" />
-                            <span className="span-style">Audi</span>
-                        </div>
-                        <div className="stat">
-                            <img src="/images/audi.png" alt="audi logo" />
-                            <span className="span-style">Audi</span>
-                        </div>
+                        {errorTopModels? (
+                            <div className="stat">
+                                <span className="Error">{errorTopBrands}</span>
+                            </div>
+                        ) : (
+                            topModels.map((model) => (
+                                <div key={model.model_name} className="stat">
+                                    <img src={model.logo} alt={model.model_name} />
+                                    <span className="span-style">{model?.model_name}</span>
+                                </div>
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
@@ -97,4 +167,4 @@ const home = () => {
     );
 };
 
-export default home;
+export default Home;
